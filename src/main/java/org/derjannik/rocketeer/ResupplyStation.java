fe32e16aaaebb61c52ef12f55e7d63adf9ac1318
Jannik <125036272+DerJanniku@users.noreply.github.com>
2024-10-16 21:23:48 +0200

    Add files via upload

---
+++ src/main/java/org/derjannik/rocketeer/ResupplyStation.java
@@ -0,0 +1,44 @@
+package org.derjannik.rocketeer;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Mob;
+import org.bukkit.inventory.ItemStack;
+import java.util.List;
+
+public class ResupplyStation {
+    private final List<Location> stations;
+
+    public ResupplyStation(List<Location> stations) {
+        this.stations = stations;
+    }
+
+    public void resupply(Mob mob) {
+        Location closestStation = this.findClosestStation(mob.getLocation(), stations);
+
+        if (closestStation != null) {
+            Block block = closestStation.getBlock();
+            if (block.getType() == Material.CHEST) {
+                ItemStack supplies = new ItemStack(Material.FIREWORK_ROCKET, 64);
+                mob.getEquipment().setItemInMainHand(supplies);
+            }
+        }
+    }
+
+    private Location findClosestStation(Location startLocation, List<Location> stations) {
+        Location closest = null;
+        double closestDistance = Double.MAX_VALUE;
+
+        for (Location station : stations) {
+            double distance = station.distance(startLocation);
+
+            if (closest == null || distance < closestDistance) {
+                closest = station;
+                closestDistance = distance;
+            }
+        }
+
+        return closest;
+    }
+}
---
+++ src/main/java/org/derjannik/rocketeer/goal/MoveToStationGoal.java
@@ -0,0 +1,39 @@
+package org.derjannik.rocketeer.goal;
+
+import com.destroystokyo.paper.entity.ai.Goal;
+import com.destroystokyo.paper.entity.ai.GoalKey;
+import com.destroystokyo.paper.entity.ai.GoalType;
+import org.bukkit.entity.Mob;
+import org.bukkit.NamespacedKey;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.EnumSet;
+
+public class MoveToStationGoal implements Goal<Mob> {
+    private static final GoalKey<Mob> KEY = GoalKey.of(Mob.class, new NamespacedKey("rocketeer", "move_to_station"));
+    private final Mob mob;
+
+    public MoveToStationGoal(Mob mob) {
+        this.mob = mob;
+    }
+
+    @Override
+    public boolean shouldActivate() {
+        return this.mob.getTarget() != null;
+    }
+
+    @Override
+    public void start() {
+        this.mob.setTarget(null);
+    }
+
+    @Override
+    public @NotNull GoalKey<Mob> getKey() {
+        return KEY;
+    }
+
+    @Override
+    public @NotNull EnumSet<GoalType> getTypes() {
+        return EnumSet.of(GoalType.MOVE, GoalType.LOOK);
+    }
+}
