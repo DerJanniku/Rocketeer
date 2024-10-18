18d7a3d6544cddedac9ba0e0b8cddcbb1c2f08c6
Jannik <125036272+DerJanniku@users.noreply.github.com>
2024-10-18 10:08:24 +0200

    Update RocketeerManager.java

--- src/main/java/org/derjannik/rocketeer/RocketeerManager.java
+++ src/main/java/org/derjannik/rocketeer/RocketeerManager.java
@@ -1,30 +1,70 @@
 package org.derjannik.rocketeer;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.entity.Piglin;
-import org.derjannik.ro cketeer.ResupplyStation;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+public class RocketeerCommand implements CommandExecutor {
 
-public class RocketeerManager {
     private final RocketeerPlugin plugin;
-    private final Map<UUID, Rocketeer> rocketeers = new HashMap<>();
-    private final ResupplyStation resupplyStation;
 
-    public RocketeerManager(RocketeerPlugin plugin) {
+    public RocketeerCommand(RocketeerPlugin plugin) {
         this.plugin = plugin;
-        this.resupplyStation = new ResupplyStation(); // Initialize resupply station
     }
 
-    public Rocketeer spawnRocketeer(Location location) {
-        Piglin piglin = location.getWorld().spawn(location, Piglin.class);
-        Rocketeer rocketeer = new Rocketeer(piglin, resupplyStation);
-        rocketeer.onSpawn();
-        rocketeers.put(piglin.getUniqueId(), rocketeer);
-        return rocketeer;
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("rocketeer")) {
+            if (args.length == 0) {
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    spawnRocketeer(player.getLocation());
+                    player.sendMessage(ChatColor.GREEN + "Rocketeer spawned!");
+                } else {
+                    sender.sendMessage(ChatColor.RED + "This command can only be used by players.");
+                }
+            } else if (args.length == 3) {
+                try {
+                    double x = Double.parseDouble(args[0]);
+                    double y = Double.parseDouble(args[1]);
+                    double z = Double.parseDouble(args[2]);
+                    Location location = new Location(((Player) sender).getWorld(), x, y, z);
+                    spawnRocketeer(location);
+                    sender.sendMessage(ChatColor.GREEN + "Rocketeer spawned at " + x + ", " + y + ", " + z + "!");
+                } catch (NumberFormatException e) {
+                    sender.sendMessage(ChatColor.RED + "Invalid coordinates. Please provide valid numbers.");
+                }
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("egg")) {
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    giveRocketeerEgg(player);
+                } else {
+                    sender.sendMessage(ChatColor.RED + "This command can only be used by players.");
+                }
+            } else {
+                sender.sendMessage(ChatColor.RED + "Usage: /rocketeer [x] [y] [z] or /rocketeer egg");
+            }
+            return true;
+        }
+        return false;
     }
 
-    // Additional methods for managing rocketeers
+    private void spawnRocketeer(Location location) {
+        plugin.getRocketeerManager().spawnRocketeer(location);
+    }
+
+    private void giveRocketeerEgg(Player player) {
+        ItemStack spawnEgg = new ItemStack(Material.PIGLIN_SPAWN_EGG);
+        ItemMeta meta = spawnEgg.getItemMeta();
+        meta.setDisplayName(ChatColor.RED + "Rocketeer");
+        spawnEgg.setItemMeta(meta);
+        player.getInventory().addItem(spawnEgg);
+        player.sendMessage(ChatColor.GREEN + "Rocketeer spawn egg added to your inventory!");
+    }
 }
