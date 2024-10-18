212a7bf3457576d0e5e6c84c8290a15eae87fdf8
Jannik <125036272+DerJanniku@users.noreply.github.com>
2024-10-18 09:58:39 +0200

    Update RocketeerPlugin.java

--- src/main/java/org/derjannik/rocketeer/RocketeerPlugin.java
+++ src/main/java/org/derjannik/rocketeer/RocketeerPlugin.java
@@ -1,25 +1,44 @@
+package org.derjannik.rocketeer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class RocketeerPlugin extends JavaPlugin {
 
-    private ResupplyStation resupplyStation;
+    private RocketeerManager rocketeerManager;
 
     @Override
     public void onEnable() {
-        // Initialize ResupplyStation
-        this.resupplyStation = new ResupplyStation(/* Add parameters as needed */);
+        // Initialize RocketeerManager
+        this.rocketeerManager = new RocketeerManager(this);
 
         // Register commands
-        this.getCommand("rocketeer").setExecutor(new RocketeerCommand(this));
-        this.getCommand("rocketeer_egg").setExecutor(new RocketeerEggCommand());
+        if (this.getCommand("rocketeer") == null) {
+            getLogger().severe("Command 'rocketeer' is not registered!");
+        } else {
+            this.getCommand("rocketeer").setExecutor(new RocketeerCommand(this));
+            getLogger().info("Command 'rocketeer' has been registered successfully.");
+        }
+
+        if (this.getCommand("rocketeer_egg") == null) {
+            getLogger().severe("Command 'rocketeer_egg' is not registered!");
+        } else {
+            this.getCommand("rocketeer_egg").setExecutor(new RocketeerEggCommand(this));
+            getLogger().info("Command 'rocketeer_egg' has been registered successfully.");
+        }
+
+        // Register event listeners
+        getServer().getPluginManager().registerEvents(new RocketeerListener(this), this);
 
         getLogger().info("Rocketeer plugin has been enabled!");
     }
 
     @Override
     public void onDisable() {
+        // Plugin shutdown logic
         getLogger().info("Rocketeer plugin has been disabled!");
     }
 
-    public ResupplyStation getResupplyStation() {
-        return this.resupplyStation;
+    public RocketeerManager getRocketeerManager() {
+        return rocketeerManager;
     }
 }
