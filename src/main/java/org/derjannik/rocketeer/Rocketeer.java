38bec94627e51edd7e5bedc2baf56891630bf429
Jannik <125036272+DerJanniku@users.noreply.github.com>
2024-10-18 09:59:53 +0200

    Update Rocketeer.java

--- src/main/java/org/derjannik/rocketeer/Rocketeer.java
+++ src/main/java/org/derjannik/rocketeer/Rocketeer.java
@@ -29,6 +29,33 @@ import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.derjannik.rocketeer.ResupplyStation;
+import org.bukkit.entity.Piglin;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.enchantments.Enchantment;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.inventory.meta.CrossbowMeta;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.Bukkit;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.derjannik.rocketeer.goal.ForgetTargetGoal;
+import org.derjannik.rocketeer.goal.MoveToStationGoal;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
+import org.bukkit.Location;
 
 public class Rocketeer {
     private final Piglin mob;
@@ -37,50 +64,18 @@ public class Rocketeer {
     private final ResupplyStation resupplyStation;
     private final UUID uuid;
 
-
     public Rocketeer(Piglin mob, ResupplyStation resupplyStation) {
         this.mob = mob;
         this.resupplyStation = resupplyStation;
         this.uuid = UUID.randomUUID();
     }
-    public Rocketeer(Piglin mob) {
-        this.mob = mob;
-        this.uuid = UUID.randomUUID();
-        this.resupplyStation = null;
-
-    }
-
-    private ItemStack createCrossbow() {
-        ItemStack crossbow = new ItemStack(Material.CROSSBOW);
-        CrossbowMeta meta = (CrossbowMeta) crossbow.getItemMeta();
-        meta.addEnchant(Enchantment.QUICK_CHARGE, 2, true);
-        meta.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
-        meta.setUnbreakable(true);
-        crossbow.setItemMeta(meta);
-        return crossbow;
-    }
-
-    private ItemStack createLeatherArmor(Material material, ChatColor color) {
-        ItemStack armor = new ItemStack(material);
-        LeatherArmorMeta meta = (LeatherArmorMeta) armor.getItemMeta();
-        meta.setColor(org.bukkit.Color.fromRGB(186, 48, 48));
-        meta.addEnchant(Enchantment.PROTECTION, 4, true);
-        meta.addEnchant(Enchantment.FEATHER_FALLING, 10, true);
-        meta.addEnchant(Enchantment.BLAST_PROTECTION, 15, true);
-        meta.addEnchant(Enchantment.PROJECTILE_PROTECTION, 15, true);
-        meta.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
-        armor.setItemMeta(meta);
-        return armor;
-    }
-
-
 
     public void onSpawn() {
         // Set attributes
         mob.setCustomName(ChatColor.BOLD + "Rocketeer");
         mob.setCustomNameVisible(true);
         mob.setPersistent(true);
-        mob.setRemoveWhenFarAway(false);
+        mob .setRemoveWhenFarAway(false);
         if (mob.getAttribute(Attribute.GENERIC_MAX_HEALTH) != null) {
             mob.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40.0);
         }
@@ -97,13 +92,17 @@ public class Rocketeer {
         mob.getEquipment().setChestplate(createLeatherArmor(Material.LEATHER_CHESTPLATE, ChatColor.DARK_RED));
         mob.getEquipment().setLeggings(createLeatherArmor(Material.LEATHER_LEGGINGS, ChatColor.DARK_RED));
         mob.getEquipment().setBoots(createLeatherArmor(Material.LEATHER_BOOTS, ChatColor.DARK_RED));
-        mob.getEquipment().getBoots().addEnchantment(Enchantment.FEATHER_FALLING, 4);
+        mob.getEquipment().getBoots().addEnchantment(Enchantment.FEATHER_FALLING, 10);
         mob.getEquipment().getBoots().addEnchantment(Enchantment.SOUL_SPEED, 10);
+
         // Initialize rockets
         for (int i = 0; i < maxRockets; i++) {
             rockets.add(new ItemStack(Material.FIREWORK_ROCKET));
         }
+
         // Add custom goal logic here
+        mob.addGoal(new ForgetTargetGoal(mob));
+        mob.addGoal(new MoveToStationGoal(mob));
     }
 
     public void onDeath() {
@@ -145,5 +144,28 @@ public class Rocketeer {
 
     public @NotNull String getName() {
         return "Rocketeer";
+    }
+
+    private ItemStack createCrossbow() {
+        ItemStack crossbow = new ItemStack(Material.CROSSBOW);
+        CrossbowMeta meta = (CrossbowMeta) crossbow.getItemMeta();
+        meta.addEnchant(Enchantment.QUICK_CHARGE, 2, true);
+        meta.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
+        meta.setUnbreakable(true);
+        crossbow.setItemMeta(meta);
+        return crossbow;
+    }
+
+    private ItemStack createLeatherArmor(Material material, ChatColor color) {
+        ItemStack armor = new ItemStack(material);
+        LeatherArmorMeta meta = (LeatherArmorMeta) armor.getItemMeta();
+        meta.setColor(org.bukkit.Color.fromRGB(186, 48, 48));
+        meta.addEnchant(Enchantment.PROTECTION, 4, true);
+        meta.addEnchant(Enchantment.FEATHER_FALLING, 10, true);
+        meta.addEnchant(Enchantment.BLAST_PROTECTION, 15, true);
+        meta.addEnchant(Enchantment.PROJECTILE_PROTECTION, 15, true);
+        meta.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
+        armor.setItemMeta(meta);
+        return armor;
     }
 }
